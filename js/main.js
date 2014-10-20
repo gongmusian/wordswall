53c01b5f04807f25ec296e2e7de1f9e662005f76
gongmusian <xp1245@hotmail.com>
2014-10-21 02:10:37 +0800

    格言牆第一版 Signed-off-by: gongmusian <xp1245@hotmail.com>

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    /*for (var i=1; i<=48; i++) {
+        $(document.body).append('<div class="element-image"><a class="element-item" rel="group" href="image/full/image-' + i + '-full.jpg"><img src="image/full/image-' + i + '-full.jpg" /></a></div>');
+    }*/
+    $(".element-item").fancybox();
+});
